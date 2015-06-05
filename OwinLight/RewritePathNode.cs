a664d660509787d15c022a73bb84d3fd5b8fde57
qldsrx <qldsrx@hotmail.com>
2015-06-05 19:55:23 +0800

    类名修改

---
+++ OwinLight/RewritePathNode.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OwinLight
+{
+    public class RewritePathNode
+    {
+        public Func<IOwinContext, string[], Task> func;
+        public Dictionary<string, RewritePathNode> chindren;
+    }
+}
